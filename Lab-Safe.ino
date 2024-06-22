b678454d741e2546af456e1a0a25e55df7699ad3
Dimas Firmansyah <100691400+msdimas@users.noreply.github.com>
2024-06-23 01:20:43 +0700

    Add files via upload

---
+++ Lab-Safe.ino
@@ -0,0 +1,29 @@
+# LabSafe
+
+## Features💡
+- This tool can detect fires accurately
+- This tool can sound an alarm and water automatically
+- This tool can send automatic messages via WA Bot and Blynk
+
+## Technology 👨‍💻
+LabSafe is created using:
+
+- [ESP32](https://camo.githubusercontent.com/85122bc4b9443e070fdce8b7b4c58783e8572e43f6cc5bc5c690846ad777d927/687474703a2f2f6769742e7768697465636174626f6172642e6f72672f646f697465737033322e6a7067) - ESP32 is used as a microcontroller.
+- [DHT22](https://id.szks-kuongshun.com/uploads/201810680/dht22-digital-temperature-humidity-sensor-module56055530906.jpg) - The DHT22 sensor is used to detect temperature and humidity.
+- [Flame Sensor](https://images.tokopedia.net/img/cache/700/product-1/2014/3/6/2827708/2827708_307f76f8-a506-11e3-ba30-2b9e4908a8c2.jpg) - Flame sensors are used to detect fire in the lab.
+- [MQ135](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTx_xKcqSuHsrcCysS6e4fxORBrHOjtVnnFw&s) - The MQ135 sensor is used to detect if there is smoke in the lab room.
+- [Mini Pump](https://jogjarobotika.com/3315/2240-3315.jpg) - Mini pumps are used for automatic watering.
+- [Buzzer](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8SYe5Y5gVDnWSfa9Q5J4f9VQ9ZtyJLfaPJ_GZW5dHoQ&s) - Buzzer is used for alarm.
+
+
+## Contributors ✨
+<br>
+<table align="center">
+  <tr>
+    <td align="center"><a href="https://github.com/bshriyuni"><img src="https://avatars.githubusercontent.com/u/114081554?v=4" width="150px;" alt=""/><br><sub><b>B16 - Besse Sahriyuni - Universitas Hasanuddin </b></sub></td> 
+    <td align="center"><a href="https://github.com/teguhkurniawan18"><img src="https://avatars.githubusercontent.com/u/167568046?v=4" width="150px;" alt=""/><br><sub><b>B17 - Teguh Kurniawan - Universitas Singaperbangsa Karawang</b></sub></td>
+    <td align="center"><a href="https://github.com/Loventina"><img src="https://avatars.githubusercontent.com/u/167601194?v=4" width="150px;" alt=""/><br><sub><b>B18 - Loventina Joshepine Prasetyo - Universitas Singaperbangsa Karawang</b></sub></td>
+    <td align="center"><a href="https://github.com/MiftahulHuda01"><img src="https://avatars.githubusercontent.com/u/134380249?v=4" width="150px;" alt=""/><br><sub><b>B19 - Miftahul Huda - Universitas Negeri Makassar </b></sub></td>
+    <td align="center"><a href="https://github.com/msdimas"><img src="https://avatars.githubusercontent.com/u/100691400?v=4" width="150px;" alt=""/><br><sub><b>B20 - Dimas Firmansyah - Universitas Udayana </b></sub></td>
+  </tr>
+</table>
